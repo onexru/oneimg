34335043045ddc275faa6580da8467b8b4d4cf6f
onexru <one@cvmail.cn>
2025-12-24 16:59:56 +0800

    初春图床v3.2

--- backend/models/User.go
+++ backend/models/User.go
@@ -2,8 +2,8 @@ package models
 
 // 用户模型
 type User struct {
-	Id       int    `gorm:"primarykey;column:id;autoIncrement" json:"id"`
+	Id       int    `gorm:"primaryKey;column:id;autoIncrement" json:"id"`
 	Role     int    `gorm:"default:1;uniqueIndex:unique_idx" json:"role"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `gorm:"unique;not null" json:"username"`
+	Password string `gorm:"not null" json:"password"`
 }
